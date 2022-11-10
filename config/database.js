bca2b9ddcc0b549d9e52b838a390908c93c4e770
AntonellaTortoza18 <antonellatortoza@gmail.com>
2022-11-10 18:05:38 -0300

    creacion de app de backEnd

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const connection = async () => {
+  try {
+    //
+    mongoose.connect(process.env.DB, 
+        // objeto con configuraciones de conexion
+        {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    console.log("connected to dabase");
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+connection();
