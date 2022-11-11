8092c51f411b6a0fc2e972fd14fd273d8c56dfe8
AntonellaTortoza18 <antonellatortoza@gmail.com>
2022-11-11 17:34:20 -0300

    MT-B1-31

--- routes/itinerary.js
+++ routes/itinerary.js
@@ -1,5 +1,6 @@
 let router = require('express').Router()
-let {read} = require("../controllers/itinerary")
+let {read, create} = require("../controllers/itinerary")
 router.get("/",read)
+router.post("/",create)
 
 module.exports = router;
