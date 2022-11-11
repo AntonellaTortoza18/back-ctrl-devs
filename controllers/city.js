876769302e944145eb1bb842e9029ed11d342fd4
AntonellaTortoza18 <antonellatortoza@gmail.com>
2022-11-11 00:20:37 -0300

    MT-01-19

--- controllers/city.js
+++ controllers/city.js
@@ -16,5 +16,40 @@ const controller = {
       });
     }
   },
+  read: async (req, res) => {
+   let query = {}
+   if(req.query.zone){
+    query ={zone: req.query.zone} 
+   }
+   if(req.query.name){
+    query = {
+      ...query,
+      name: { $regex: req.query.name, $options: 'i' },
+    }
+   }
+    try {
+     
+      let all_cities = await City.find(query) 
+      if(all_cities){
+        res.status(200).json({
+          success: true,
+          message: "the cities were successfully found",
+          response: all_cities,
+        })
+      } else{
+        res.status(404).json({
+          success: false,
+          message: "there are no cities",
+        
+        })
+      }
+       
+    } catch (error) {
+      res.status(400).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  },
 }
 module.exports = controller;
