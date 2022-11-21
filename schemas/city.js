d84a2c98769b610f998f07944ac62b8bef6c7a95
AntonellaTortoza18 <antonellatortoza@gmail.com>
2022-11-21 19:31:28 -0300

    MT-01-39

--- schemas/city.js
+++ schemas/city.js
@@ -2,25 +2,28 @@ const joi = require("joi");
 
 const schema = joi.object({
   name: joi.string().required().min(3).max(20).messages({
-    "string.base": "enter the name of the city please",
-    "any.required": "complete this input, please",
-    "string.empty": "complete the name of the city, please",
-    "string.min":"min three characters",
-    "string.max":"max twenty characters"
+    "string.base": "Enter the name of the city please",
+    "any.required": "The field 'Name of the city' is required, please complete it",
+    "string.empty": "Complete the field 'Name of the city', please",
+    "string.min": "Name: min 3 characters",
+    "string.max": "Name: max 20 characters",
   }),
   zone: joi.string().required().min(3).max(15).messages({
-    "any.required": "complete this input, please",
-    "string.empty": "complete the zone of city, please",
-    "string.min":"min three characters",
-    "string.max":"max fifteen characters"
+    "string.base": "Enter the zone of the city please",
+    "any.required": "The field 'Zone of city' is required, please complete it",
+    "string.empty": "Complete the field 'Zone of city', please",
+    "string.min": "Zone: min 3 characters",
+    "string.max": "Zone: max 15 characters",
   }),
   photo: joi.string().required().uri().messages({
-    "any.required": "complete this input, please",
-    "string.empty": "complete the URL photo, please",
+    "any.required": "The field 'URL photo' is required, please complete it",
+    "string.empty": "Complete the field 'URL photo', please",
+    "string.uri": "The field 'URL photo' must be an url",
   }),
-  population: joi.number().required().min(500).max(10000000000).messages({
-    "number.empty": "enter the number of the population please",
-    "number.base": "enter the number of the population please",
+  population: joi.number().required().messages({
+    "number.empty": "Complete the field 'Population' please",
+    "number.base": "The field 'Population' must be a number",
+    "any.required": "The field 'Population' is required, please complete it",
   }),
   userId: joi.any(),
 });
