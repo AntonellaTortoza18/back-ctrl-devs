7d87f48c7eb60ee4d3446af37474b9ec5fb5b970
AndreaCastanoS <andreacastsalazar@gmail.com>
2022-11-30 13:36:26 -0500

    MT-02-57

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ let city = require("./city")
 let itinerary = require("./itinerary")
 let hotel = require("./hotel")
 let show = require("./show")
+
+let comment = require ("./comment")
 // ubi de las rutas de curso.
 
 // le obligo al enrutador principal que use la palabra user para poder controlar las rutas de user.
@@ -17,5 +19,9 @@ router.use('/api/itineraries', itinerary)
 
 router.use('/api/hotels', hotel)
 router.use('/api/shows', show)
+
+
+router.use('/api/comments', comment)
+
 
 module.exports = router;
